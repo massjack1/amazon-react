083c1b4ac12d350069cef7721604bacc58b8551f
Mass Jack <mass.jack4@gmail.com>
2023-03-06 09:00:13 +0100

    Work on the checkout component

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from "next/image";
+import { StarIcon } from '@heroicons/react/outline';
 
 function CheckoutProduct({ 
     id,
@@ -11,8 +13,18 @@ function CheckoutProduct({
     hasPrime, 
 }) {
   return (
-    <div>
-        
+    <div className='grid grid-cols-5'>
+        <Image src ={image} height={200} width={200} object-fit="contain"
+        />
+        {/*Middle*/}
+        <div className='col-span-3 mx-5'>
+          <p>{title}</p>
+          <div className="flex">
+            {Array(rating).fill().map((_,i) => (
+                <StarIcon key={i} className="h-5 text-yellow-500"/>
+            ))}
+        </div>
+        </div>
     </div>
   )
 }
