78a21844c9794ea3560aa1aac2c009ccb520f642
Mass Jack <mass.jack4@gmail.com>
2023-03-09 19:19:38 +0100

    Fix Checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,9 +3,13 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 import { selectItems } from "@/slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
+
 
 function Checkout() {
     const items = useSelector(selectItems);
+    const { data: session } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -44,6 +48,24 @@ function Checkout() {
                 </div>
 
             {/* Right */}
+            <div>
+                {items.length > 0 && (
+                    <>
+                    <h2 className="whitespace-nowrap">Subtotal ({items.length} items): 
+                    <span className="font-bold">
+                        {/* <Currency quantity={total} currency="GBP"/> */}
+                    </span> 
+                    </h2>
+
+                    <button className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}>
+                        {!session ? "Sign in to checkout" : "Proceed to checkout"}
+                    </button>
+                    </>
+                )}
+            </div>
         </main>
     </div>
   )
